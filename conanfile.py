5e4567f06900f0400ee3c5baaa53aa57654876a8
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-09-12 02:21:56 -0400

    chore: release 1.4.1 pinned to the published sdk revisions

--- conanfile.py
+++ conanfile.py
@@ -21,7 +21,7 @@ class VoltModConan(ConanFile):
 
     name = "voltmod"
     author = "Sukhrob Ilyosbekov (suxrobgm@gmail.com)"
-    version = "1.4.0"
+    version = "1.4.1"
     description = "C++23 library for CS2 Metamod:Source plugins"
     license = "MIT"
     homepage = "https://github.com/voltygg/voltmod"
