6f6ff4dc788dbf287f64dc3c10209a625bf5c577
Sukhrob Ilyosbekov <suxrobgm@gmail.com>
2026-09-25 09:10:55 -0400

    style: apply clang-format

--- src/Hooks/GlowVision.cpp
+++ src/Hooks/GlowVision.cpp
@@ -97,8 +97,8 @@ void GlowVision::Refresh()
 
         if (pair.Active())
         {
-            bool stale = !desired || team != pair.Team || !_entities.Get(pair.Relay) ||
-                         !_entities.Get(pair.Glow) || pawn.ModelName() != pair.Model;
+            bool stale = !desired || team != pair.Team || !_entities.Get(pair.Relay) || !_entities.Get(pair.Glow) ||
+                         pawn.ModelName() != pair.Model;
             if (stale)
             {
                 DestroyPair(pair);
